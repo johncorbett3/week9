d082c3314f82fc3601bb3e691dc80a1bdf0de555
John Corbett <john.corbett@kellogg.northwestern.edu>
2021-03-11 16:26:21 -0600

    netlify functions ugh

--- netlify/functions/create_post.js
+++ netlify/functions/create_post.js
@@ -10,18 +10,47 @@ exports.handler = async function(event) {
   //         in the POST request, and assign to variables. Use 
   //         console.log if necessary, to ensure the values are what
   //         you're expecting.
+
+  let body = JSON.parse(event.body)
+  
+  let userId = body.userId
+  let userName = body.userName
+  let imageUrl = body.imageUrl
+  
+    console.log("post id is ${userName}")
+    console.log("post id is ${userId}")
+
+
   // Step 3: Construct an object of data to send to Firestore – this
   //         object should include the user ID, username, image URL,
   //         and a "created" timestamp – can use the built-in function
   //         for this – firebase.firestore.FieldValue.serverTimestamp()
+
+  let newPost = {
+    userId: userId,
+    username: userName,
+    imageUrl: imageUrl,
+    created: firebase.firestore.FieldValue.serverTimestamp()
+  }
+
+  let docRef = await db.collection("posts").add(newPost)
+
+
+
   // Step 4: Add the post to Firestore using the .add() function.
   // Step 5: Add the newly created post's auto-generated ID to the object
   //         you created in Step 3, and return that entire object as the
   //         body of the return value, using JSON.stringify()
+let postId = docRef.id 
+
+newPost.postId  = postId
+
+console.log(newPost)
+
 
   return {
     statusCode: 200,
-    body: JSON.stringify({})
+    body: JSON.stringify(newPost)
   }
 
 }
