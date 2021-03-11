34c90fbe045c705f8ee65e41b15fd71411a0049a
John Corbett <john.corbett@kellogg.northwestern.edu>
2021-03-11 16:26:12 -0600

    netlify

--- kelloggram.js
+++ kelloggram.js
@@ -22,6 +22,15 @@ firebase.auth().onAuthStateChanged(async function(user) {
       // Step 1:   POST fetch the create_post endpoint. Send the currently logged-in
       //           user's uid and username, and the image URL from the form in the 
       //           POST request's body.
+      let response = await fetch('/.netlify/functions/create_posts')
+        method: "POST"
+        body: JSON.stringify({
+            userId: user.uid,
+            userName: postUsername,
+            imageUrl: postImageUrl,
+
+        })
+
       // Step 2-5: Implement the lambda function in create_post.js
       // Step 6:   The lambda should return an Object of data with information on the
       //           the post, including the newly created post's ID. Set this to the 
